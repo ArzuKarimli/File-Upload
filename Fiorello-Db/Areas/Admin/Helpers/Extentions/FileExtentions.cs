3e369ba3e2b0aad28285c465644ae991d80ad7ac
arzusk <arzusk@code.edu.az>
2024-05-18 13:34:08 -0700

    Add project files.

---
+++ Fiorello-Db/Areas/Admin/Helpers/Extentions/FileExtentions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.IO;
+
+namespace Fiorello_Db.Areas.Admin.Helpers.Extentions
+{
+    public static class FileExtentions
+    {
+      public static bool CheckFileSize(this IFormFile file,int size)
+        {
+            return file.Length / 1024 < size;
+        }
+        public static bool CheckFileType(this IFormFile file,string pattern)
+        {
+            return file.ContentType.Contains(pattern);
+        }
+        public async static Task SaveFileToLocalAsync(this IFormFile file,string path)
+        {
+            using FileStream stream = new FileStream(path, FileMode.Create);
+            file.CopyToAsync(stream);
+        }
+    }
+}
---
+++ Fiorello-Db/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Fiorello_Db.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Fiorello_Db.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly IBioService _bioService;
+
+        public HeaderViewComponent(IBioService bioService)
+        {
+            _bioService = bioService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await _bioService.GetAllAsync();
+            return View(model);
+        }
+    }
+}
